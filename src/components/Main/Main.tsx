e8f635acb2ae39057337cfa0fe058bc1f34691ee
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-13 15:54:26 +0100

    ADDED Main-content component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import styles from "./Main.module.scss";
+
+export default function Main({ children }: { children: ReactNode }) {
+  return <div className={styles.container}>{children}</div>;
+}
