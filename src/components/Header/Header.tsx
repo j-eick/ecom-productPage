4870a3c742fe1bf98ef546cf061d8ed47cc3200e
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-13 15:53:44 +0100

    ADDED reusable header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,9 @@
-export default function Header({ children }) {
-  return <div>{children}</div>;
+import styles from "./Header.module.scss";
+
+type headerProps = {
+  children: React.ReactNode;
+};
+
+export default function Header({ children }: headerProps) {
+  return <div className={styles.header}>{children}</div>;
 }
