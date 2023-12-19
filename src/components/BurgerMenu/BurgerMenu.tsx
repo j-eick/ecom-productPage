8b60c8f7d45d473249c2bba8088d2c624ca0e65a
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-19 23:50:28 +0100

    burger styling

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -7,13 +7,7 @@ type burgerProps = {
 export default function BurgerMenu({ onClick }: burgerProps) {
   return (
     <div className={styles.burgerMenu}>
-      <img
-        src="/images/icon-menu.svg"
-        alt="burger_menu"
-        width="18"
-        style={{ display: "flex" }}
-        onClick={onClick}
-      />
+      <img src="/images/icon-menu.svg" alt="burger_menu" onClick={onClick} />
     </div>
   );
 }
