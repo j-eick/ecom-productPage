1a21a08329866e0ae76cd7c078fef60eb5e325b8
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-28 16:40:52 +0100

    renaming Header files

--- src/components/HeaderMobile/HeaderMobile.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import styles from "./HeaderMobile.module.scss";
-
-export default function HeaderMobile({ children }: { children: ReactNode }) {
-  return <div className={styles.container}>{children}</div>;
-}
