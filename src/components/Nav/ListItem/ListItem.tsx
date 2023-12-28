641a00969d6daecda14c9c722f0dac6a5b3c813d
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-28 16:44:12 +0100

    import classNames package; minuscule stuff

--- src/components/Nav/ListItem/ListItem.tsx
+++ src/components/Nav/ListItem/ListItem.tsx
@@ -1,6 +1,21 @@
 import { ReactNode } from "react";
 import styles from "./ListItem.module.scss";
+import classNames from "classnames";
 
-export default function ListItem({ children }: { children: ReactNode }) {
-  return <div className={styles.listItem}>{children}</div>;
+type listItemProps = {
+  children: ReactNode;
+  isSideMenuOpen: boolean;
+};
+
+export default function ListItem({ children, isSideMenuOpen }: listItemProps) {
+  const styleClasses = classNames(styles.listeItem, {
+    [styles.close]: !isSideMenuOpen,
+    [styles.open]: isSideMenuOpen,
+  });
+
+  return (
+    <div className={styleClasses} data-sidemenu={isSideMenuOpen}>
+      {children}
+    </div>
+  );
 }
