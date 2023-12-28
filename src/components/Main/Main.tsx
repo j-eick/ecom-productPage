d5d6b17f0eebe373bfb5487637fc95db92823a57
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-28 16:42:57 +0100

    cleaning up

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from "react";
 import styles from "./Main.module.scss";
 
 export default function Main({ children }: { children: ReactNode }) {
-  return <div className={styles.container}>{children}</div>;
+  return <div className={styles.main}>{children}</div>;
 }
