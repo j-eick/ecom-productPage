bd26069dc6c4bd6aa72f2177fd6ca59c6b4c12a7
Joschka Eickhoff <jeickhoff@posteo.de>
2024-01-04 18:31:55 +0100

    fixed sideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ import UList from "./components/Nav/UList/UList";
 import ListItem from "./components/Nav/ListItem/ListItem";
 import navElements from "../src/util/nav.json";
 import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
+import useWindowResize from "./hooks/useWindowResize";
 import SideMenu from "./components/SideMenu/SideMenu";
 import { useState } from "react";
 import { SVGstorage } from "./util/SVGstorage";
 import styles from "./components/SideMenu/Sidemenu.module.scss";
 import Image from "./components/Image/Image";
+// import HeaderDesktop from "./components/HeaderDesktop/HeaderDesktop";
 
-console.clear();
+// console.clear();
 
 function App() {
   const [isSidemenuOpen, setIsSidemenuOpen] = useState<boolean>(false);
@@ -22,28 +24,26 @@ function App() {
   return (
     <PageContainer>
       <Main>
-        <Header>
-          <SideMenu isSideMenuOpen={isSidemenuOpen}>
-            <SVGstorage
-              className={styles.back_icon}
-              variant="cancel"
-              size="32px"
-              color="#111418"
-              viewBox="0 0 470 1000"
-              onClick={() => setIsSidemenuOpen(!isSidemenuOpen)}
-            />
+        <SideMenu isSideMenuOpen={isSidemenuOpen}>
+          <SVGstorage
+            className={styles.back_icon}
+            variant="cancel"
+            size="32px"
+            color="#111418"
+            viewBox="0 0 470 1000"
+            onClick={() => setIsSidemenuOpen(!isSidemenuOpen)}
+          />
 
-            <UList>
-              {navElements.map((navItem) => (
-                <ListItem
-                  isSideMenuOpen={isSidemenuOpen}
-                  key={navItem.category}
-                >
-                  {navItem.category}
-                </ListItem>
-              ))}
-            </UList>
-          </SideMenu>
+          <UList>
+            {navElements.map((navItem) => (
+              <ListItem isSideMenuOpen={isSidemenuOpen} key={navItem.category}>
+                {navItem.category}
+              </ListItem>
+            ))}
+          </UList>
+        </SideMenu>
+
+        <Header>
           <Nav>
             <BurgerMenu
               onClick={() => {
